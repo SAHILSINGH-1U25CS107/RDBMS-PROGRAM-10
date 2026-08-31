815fdb153e26468e28c576b255280a99a97ec1bf
SAHILSINGH-1U25CS107 <sahilto482000@gmail.com>
2026-08-31 12:13:34 +0530

    Update starter.sql

--- starter.sql
+++ starter.sql
@@ -1,43 +1,36 @@
-create database college2;
-use college2;
-
-create table course(courseid integer(5),coursename varchar(30),credits integer(5));
-desc course;
-insert course values ("201","Database system","4"),("202","Data structure","3"),("203","Mathematics","4");
-select * from course;
-
-create table enrollment(enrollmentid integer(5),studentid integer(5),courseid integer(5));
-desc enrollment;
-insert enrollment values("1","1001","201"),("2","1001","202"),("3","1002","203"),("4","1003","201");
-select * from enrollment;
-
-select course.coursename,course.credits,enrollment.enrollmentid,enrollment.studentid from course left join enrollment on course.courseid=enrollment.enrollmentid;
+CREATE TABLE Course (
+    CourseID INT PRIMARY KEY,
+    CourseName VARCHAR(100),
+    Credits INT
+);
 
 
+desc Course;
+
+CREATE TABLE Enrollment (
+    EnrollmentID INT PRIMARY KEY,
+    StudentID INT,
+    CourseID INT
+);
+
+
+desc Enrollment;
+
+INSERT INTO Course (CourseID, CourseName, Credits)
+VALUES
+(201, 'Database Systems', 4),
+(202, 'Data Structures', 3),
+(203, 'Mathematics', 4);
 
 
 
 
+INSERT INTO Enrollment (EnrollmentID, StudentID, CourseID)
+VALUES
+(1, 1001, 201),
+(2, 1001, 202),
+(3, 1002, 203),
+(4, 1003, 201);
 
-
-
-
-
-
-
-create database college2;
-use college2;
-
-create table course(courseid integer(5),coursename varchar(30),credits integer(5));
-desc course;
-insert course values ("201","Database system","4"),("202","Data structure","3"),("203","Mathematics","4");
-select * from course;
-
-create table enrollment(enrollmentid integer(5),studentid integer(5),courseid integer(5));
-desc enrollment;
-insert enrollment values("1","1001","201"),("2","1001","202"),("3","1002","203"),("4","1003","201");
-select * from enrollment;
-
-select course.coursename,course.credits,enrollment.enrollmentid,enrollment.studentid from course right join enrollment on course.courseid=enrollment.enrollmentid;
-
-
+select Course.CourseName,Course.Credits,Enrollment.EnrollmentID,Enrollment.StudentID from Course left join Enrollment on Course.CourseID=Enrollment.CourseID;
+select Course.CourseName,Course.Credits,Enrollment.EnrollmentID,Enrollment.StudentID from Course right join Enrollment on Course.CourseID=Enrollment.CourseID;
